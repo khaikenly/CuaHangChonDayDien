d4f6aba1f9eaf471fe60b1dd0b452b47920af897
khaikenly <bet365yasuo@gmail.com>
2020-06-07 21:48:49 +0700

    test

--- app/src/main/java/com/example/cuahangchondaydien/activity/MainActivity.java
+++ app/src/main/java/com/example/cuahangchondaydien/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.cuahangchondaydien.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.ViewFlipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cuahangchondaydien.R;
 import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +30,46 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerViewmanhinhchinh;
     NavigationView navigationView;
     ListView listViewmanhinhchinh;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        ArrayList<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("https://kythuatchetao.com/wp-content/uploads/2015/01/day-dong.jpg");
+        mangquangcao.add("https://i.ytimg.com/vi/VoUVLrxIuY4/maxresdefault.jpg");
+        mangquangcao.add("https://maybompanasonic.net/kcfinder/upload//images/photo1529374652587-15293746525871459256669.jpg");
+        mangquangcao.add("https://file1.hutech.edu.vn/file/editor/homepage1/Phoi%20canh%20co%20so%20Hutech_co%20so%20DBP%20%20.jpg");
+        for (int i = 0; i < mangquangcao.size(); i++) {
+            ImageView imageView = new ImageView(getApplicationContext());
+            Picasso.get().load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        Animation animation_slide_in = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation animation_slide_out = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.silde_out_right);
+        viewFlipper.setInAnimation(animation_slide_in);
+        viewFlipper.setOutAnimation(animation_slide_out);
+    }
+
+    private void ActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     private void Anhxa() {
@@ -31,5 +78,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewmanhinhchinh = findViewById(R.id.recyclerview);
         navigationView = findViewById(R.id.recyclerview);
         listViewmanhinhchinh = findViewById(R.id.listviewmanhinhchinh);
+        drawerLayout = findViewById(R.id.drawerlayout);
     }
 }
